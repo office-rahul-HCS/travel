a96de6573f945b2cee6315846196e4eabe9c6b69
Rachana <rachana@honeycombsoftwares.com>
2021-09-02 10:20:36 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var btn = document.querySelector(".btn_load");
+var txt = document.querySelector("#text")
+console.log(btn)
+
+
+
+btn.addEventListener("click",function(){
+    var x = " ";
+    console.log(x)
+    // var x =txt.innerText;
+    console.log(x)
+    txt.innerText=x;
+
+})
